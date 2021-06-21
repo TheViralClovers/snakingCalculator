896aac92cc2350cb8df1adb33f07c08efb1e4248
TheViralClovers <theviralclovers@gmail.com>
2021-06-21 19:13:45 +0530

    added total fixed bug
    Now the total will also be displayed in the table, a bug which caused the app to crash if you had an unopened pack has been removed, should work smoothly now

--- snakingCalculator.py
+++ snakingCalculator.py
@@ -21,7 +21,6 @@ checkComplete = 0
 owner = input("Enter your wax address ")
 
 complete_raw_data = []
-# response = requests.get("https://wax.api.atomicassets.io/atomicassets/v1/assets?owner=k3bri.wam&collection_name=novarallywax&page=1&limit=9999999999&order=desc&sort=asset_id")
 
 while checkComplete == 0:
     response = requests.get(f"https://wax.api.atomicassets.io/atomicassets/v1/assets?owner={owner}&collection_name=novarallywax&page={page}&limit=1000&order=desc&sort=asset_id")
@@ -33,27 +32,34 @@ while checkComplete == 0:
     else:
         checkComplete = 1
         
-# print(len(complete_raw_data))
+# print(complete_raw_data)
 
 for i in range(len(complete_raw_data)):
-    rarity = complete_raw_data[i]["template"]["immutable_data"]["Rarity"]
-    if rarity == "Common":
-        common_count += 1
-    if rarity == "Uncommon":
-        uncommon_count += 1
-    if rarity == "Rare":
-        rare_count += 1
-    if rarity == "Classic":
-        classic_count += 1
-    if rarity == "Sketch":
-        sketch_count += 1
+    try:
+        rarity = complete_raw_data[i]["template"]["immutable_data"]["Rarity"]
+        if rarity == "Common":
+            common_count += 1
+        if rarity == "Uncommon":
+            uncommon_count += 1
+        if rarity == "Rare":
+            rare_count += 1
+        if rarity == "Classic":
+            classic_count += 1
+        if rarity == "Sketch":
+            sketch_count += 1
+    except:
+        pass
+
+total_snake = (common_count*common_snake) + (uncommon_count*uncommon_snake) + (rare_count*rare_snake) + (classic_count*classic_snake) + (sketch_count*sketch_snake)
+total_count = common_count+uncommon_count+rare_count+classic_count+sketch_count
 
 common_row = list(("COMMON",common_count,(common_count*common_snake),(common_count*common_snake/10)))
 uncommon_row = list(("UNCOMMON",uncommon_count,(uncommon_count*uncommon_snake),(uncommon_count*uncommon_snake/10)))
 rare_row = list(("RARE",rare_count,(rare_count*rare_snake),(rare_count*rare_snake/10)))
 classic_row = list(("CLASSIC",classic_count,(classic_count*classic_snake),(classic_count*classic_snake/10)))
 sketch_row = list(("SKETCH",sketch_count,(sketch_count*sketch_snake),(sketch_count*sketch_snake/10)))
-final_table_list = list((common_row,uncommon_row,rare_row,classic_row,sketch_row))
+total_row = list(("TOTAL",total_count, total_snake, (total_snake/10)))
+final_table_list = list((common_row,uncommon_row,rare_row,classic_row,sketch_row,total_row))
 
 # print(final_table_list)
 
@@ -63,7 +69,6 @@ print("\n")
 
 print(table)
 
-total_snake = (common_count*common_snake) + (uncommon_count*uncommon_snake) + (rare_count*rare_snake) + (classic_count*classic_snake) + (sketch_count*sketch_snake)
 # print(total_snake)
 
 print(f"\nYou will be making {total_snake} Snake Oil per day and {total_snake/10} Snake Oil per day before launch\nMade by TheViralClovers/k3bri.wam")
